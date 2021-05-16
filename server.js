0fec76f2a52a4702061e5cc78d939974e532c784
Wayne Edwards <waedwards42@yahoo.com>
2021-05-16 16:53:28 -0700

    added functions and inquirer

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-const express = require('express');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const cTable = require('console.table');
 const inputCheck = require('./utils/inputCheck');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection({
@@ -21,11 +15,143 @@ const db = mysql.createConnection({
     console.log('Connected to the employee_tracker database.')
 );
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-});
+const questions = [{
+        type: 'list',
+        name: 'options',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Departments',
+            'View All Roles',
+            'View All Employees',
+            'Add A Department',
+            'Add A Role',
+            'Add A Employee',
+            'Update An Employee Role'
+        ]
+    },
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+    {
+        type: 'input',
+        name: 'newDepartmentName',
+        message: 'What Is The Name Of Your Department?',
+        when: (answers) => {
+            if (answers.options == 'Add A Department') {
+                return true
+            } else {
+                return false
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'addNewRole',
+        message: 'What Is The name Of The New Role?',
+        when: (answers) => {
+            if (answers.options == 'Add A Role') {
+                return true
+            } else {
+                return false
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'addNewEmployee',
+        message: 'What Is The Name Of The New Employee?',
+        when: (answers) => {
+            if (answers.options == 'Add An Employee') {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+];
+
+
+
+function myAnswers(answers) {
+    switch (answers.options) {
+        case 'View All Departments':
+            db.promise()
+                .query('SELECT * FROM department')
+                .then(([rows, fields]) => {
+                    let results = cTable.getTable(rows);
+                    console.log(results);
+                    restart()
+
+                })
+
+            break;
+
+        case 'View All Roles':
+            console.log('View All Roles')
+            db.query('SELECT * FROM role', (err, results, fields) => {
+                let roleResults = cTable.getTable(results);
+                console.log(roleResults)
+            })
+            break;
+
+        case 'View All Employees':
+            console.log('View All Employees')
+            db.query('SELECT * FROM employee', (err, rows, fields) => {
+                let employeeResults = cTable.getTable(rows);
+                console.log(employeeResults)
+            })
+            break;
+
+
+
+        case 'Add A Department':
+            console.log('Add A Department')
+            db.promise()
+                .query(`INSERT INTO department (name) VALUES('${answers.newDepartmentName}')`)
+                .then(([rows, fields]) => {
+                    console.log('added a department')
+
+                })
+            break;
+
+        case 'Add A Role':
+            console.log('Add A Role')
+            db.promise()
+                .query(`INSERT INTO role (title, salary, department_id) VALUES('${answers.addNewRole}')`)
+                .then(([rows, fields]) => {
+                    console.log('added a role')
+                })
+            break;
+
+        case 'Add A Employee':
+            console.log('Add A Employee')
+            db.promise()
+                .query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES('${answers.addNewEmployee}')`)
+                .then(([rows, fields]) => {
+                    console.log('added an employee')
+
+                })
+            break;
+
+        case 'Update An Employee Role':
+            console.log('Update An Employee Role')
+            break;
+
+
+        default:
+            break;
+    }
+}
+
+function employeeTracker() {
+    inquirer
+        .prompt(questions)
+        .then(myAnswers)
+
+}
+
+function restart() {
+    employeeTracker()
+}
+
+employeeTracker()
